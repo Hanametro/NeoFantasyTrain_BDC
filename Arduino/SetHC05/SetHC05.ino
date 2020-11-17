80a335aa08d5dcd84b46930fe32a71e7a8941430
Hanametro <61606826+Hanametro@users.noreply.github.com>
2020-11-17 11:07:07 +0800

    Add files via upload

---
+++ Arduino/SetHC05/SetHC05.ino
@@ -0,0 +1,52 @@
+//Attention!This Program SHOULD BE USED Under AT mode!
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(38400);
+ 
+}
+
+void sendcmd()
+{
+    Serial.println("AT");
+  while(Serial.available())
+  {
+    char ch;
+    ch = Serial.read();
+    Serial.print(ch);
+  } // Get response: OK
+  delay(1000); // wait for printing 
+
+  
+  Serial.println("AT+NAME=BR182-01");
+  while(Serial.available())
+  {
+    char ch;
+    ch = Serial.read();
+    Serial.print(ch);
+  }
+  delay(1000);
+
+  Serial.println("AT+ADDR?");
+  while(Serial.available())
+  {
+    char ch;
+    ch = Serial.read();
+    Serial.print(ch);
+  }
+  delay(1000);
+
+  Serial.println("AT+PSWD=0000");
+  while(Serial.available())
+  {
+    char ch;
+    ch = Serial.read();
+    Serial.print(ch);
+  }
+  delay(1000);
+  
+}
+
+
+void loop() {
+    sendcmd();
+}
